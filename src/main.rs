cff7b01f7645f71a6c8bf9e4cba3e12c6253333c
Vaibhav soni <32386700+white-hat-vaibhs@users.noreply.github.com>
2021-05-20 02:22:41 -0700

    Day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// mod print;
+// mod vars;
+mod types;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    // print::run();
+    // vars::run();
+    types::run();
 }
